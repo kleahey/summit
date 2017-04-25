7ab92ee809184853bd2f5a93608f6dd320c22ccf
Kevin Leahey <kleahey@me.com>
2017-04-25 11:08:18 -0400

    Team Members added to seeds list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+teammember_list = [
+  "Jenny Ricard",
+  "Lesley Hargreaves",
+  "Chad Massie",
+  "Aba Blankson",
+  "Scott Anderson",
+  "Tanya Fox",
+  "Jennifer Dart",
+  "Jen Davis",
+  "Kevin Leahey",
+  "Elisabeth Hancock",
+  "Aisha Johnson",
+  "Annie Unger",
+  "Ariana Ortega",
+  "Camille Green",
+  "Caryn Brock",
+  "Jessica Joseph",
+  "Kyle Fredericks",
+  "Matt Halterman",
+  "Michael Moreland",
+  "Neal Pettingill",
+  "Ryan Gleich",
+  "Ryan Hydock",
+  "Sarah Knapp",
+  "Tara Kuhn",
+  "Tracey Shaup",
+  "Wendy Guevara",
+  "Jen Gallihugh",
+  "Miriam Hancock",
+  "Andy Hahn",
+  "Michele Paganin",
+  "Tim Weber",
+  "Yezan Abdul-Razeq",
+  "Adam Tate",
+  "Amy Merhige",
+  "Dana Mikelson",
+  "Matt McCabe",
+  "Darell Boseman",
+  "Deryn Pomeroy",
+  "Fatimeh Rajabi",
+  "Greg Sluck",
+  "Joanna Cleary",
+  "Ryan Bui",
+  "Alice Selighson",
+  "Carole Mitchell",
+  "Ignacio Noboa",
+  "Kyle Bourgeois"
+]
+
+teammember_list.each do |name|
+  Teammember.create( name: name )
+end

8656c58adb2c61037e1940983fef9c0d9e3e389f
Kevin Leahey <kleahey@me.com>
2017-04-27 15:57:01 -0400

    fix errors

--- app/controllers/message_controller.rb
+++
@@ -1,9 +0,0 @@
-require 'hipchat'
-
-class MessageController < ApplicationController
-
-  def hipchat
-    @communication = Hipchat.post_communication(params[:color], params[:message], params[:notify], params[:message_format])
-  end
-
-end

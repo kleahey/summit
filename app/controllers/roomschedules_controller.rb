6b47ed96fd541be66c23f7d5f7872b9a158b442b
Kevin Leahey <kleahey@me.com>
2017-04-27 20:44:42 -0400

    Room staffing functionality active

--- app/controllers/roomschedules_controller.rb
+++ app/controllers/roomschedules_controller.rb
@@ -4,4 +4,11 @@ class RoomschedulesController < ApplicationController
     @rooms = Session.all.order(name: :asc)
   end
 
+  def list
+    @room = Session.find(params[:session_id])
+    @roominfo = Schedule.where(session_id: params[:session_id])
+  end
+
+  private
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'roomschedules', to: "roomschedules#index"
+  get 'roomschedules/:session_id/list', as: "roomschedules_list", to: "roomschedules#list"
+
   resources :sessions
   resources :roles
   resources :locations

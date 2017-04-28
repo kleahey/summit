8a8e0ede8f00a79218c266c13a8ab0f5263a72fc
Kevin Leahey <kleahey@me.com>
2017-04-28 13:17:21 -0400

    Updated Admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   get 'roomschedules', to: "roomschedules#index"
   get 'roomschedules/:session_id/list', as: "roomschedules_list", to: "roomschedules#list"
 
-  resources :sessions
-  resources :roles
-  resources :locations
+  get 'admin', to: "admin#index"
+  get 'admin/teammembers', to: "teammembers#index"
+  get 'admin/teammembers/:teammember_id/schedules', to: "schedules#index"
+
+  scope '/admin' do
+    resources :sessions, :locations, :roles
+  end
+
   resources :teammembers do
     resources :schedules
   end

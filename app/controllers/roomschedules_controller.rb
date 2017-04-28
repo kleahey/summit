b5e5ef6788f797088e6c47fb16b8ce755f54ca7a
Kevin Leahey <kleahey@me.com>
2017-04-28 08:55:09 -0400

    Link to session schedules from team member schedule

--- app/controllers/roomschedules_controller.rb
+++ app/controllers/roomschedules_controller.rb
@@ -7,6 +7,7 @@ class RoomschedulesController < ApplicationController
   def list
     @session = Session.find(params[:session_id])
     @schedules = Schedule.where(session_id: params[:session_id]).order(role_id: :desc)
+    @dates = Schedule.select(:start_time).where(session_id: params[:session_id]).group(:start_time)
   end
 
   private

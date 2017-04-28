f23e8f7f3053426b3e7f40b73df2ad2d64a51b8b
Kevin Leahey <kleahey@me.com>
2017-04-27 21:05:48 -0400

    Order roles on room schedule pages

--- app/controllers/roomschedules_controller.rb
+++ app/controllers/roomschedules_controller.rb
@@ -1,12 +1,12 @@
 class RoomschedulesController < ApplicationController
 
   def index
-    @rooms = Session.all.order(name: :asc)
+    @sessions = Session.all.order(name: :asc)
   end
 
   def list
-    @room = Session.find(params[:session_id])
-    @roominfo = Schedule.where(session_id: params[:session_id])
+    @session = Session.find(params[:session_id])
+    @roominfo = Schedule.where(session_id: params[:session_id]).order(role_id: :desc)
   end
 
   private
